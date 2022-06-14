20197e1f5b87ce32a738ec28ac0d5a12f4384966
hmupfumi <h.mupfumi@delta.co.zw>
2022-06-14 07:21:23 +0200

    whole project

--- src/app/aboutus/aboutus.page.spec.ts
+++ src/app/aboutus/aboutus.page.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { HomePage } from './home.page';
+import { AboutusPage } from './aboutus.page';
 
-describe('HomePage', () => {
-  let component: HomePage;
-  let fixture: ComponentFixture<HomePage>;
+describe('AboutusPage', () => {
+  let component: AboutusPage;
+  let fixture: ComponentFixture<AboutusPage>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomePage ],
+      declarations: [ AboutusPage ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HomePage);
+    fixture = TestBed.createComponent(AboutusPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,39 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'welcome',
+    pathMatch: 'full'
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
+  // { path: '', loadChildren: () => import('./tebs/tebs.module').then( m => m.TebsPageModule) },
+  // { path: 'teb1', loadChildren: () => import('./teb1/teb1.module').then( m => m.Teb1PageModule) },
+  // { path: 'teb3', loadChildren: () => import('./teb3/teb3.module').then( m => m.Teb3PageModule) },
+  {
+    path: 'aboutus',
+    loadChildren: () => import('./aboutus/aboutus.module').then( m => m.AboutusPageModule)
+  },
+  {
+    path: 'contactus',
+    loadChildren: () => import('./contactus/contactus.module').then( m => m.ContactusPageModule)
+  },
+  {
+    path: 'helpus',
+    loadChildren: () => import('./helpus/helpus.module').then( m => m.HelpusPageModule)
+  },
+  {
+    path: 'report-incident',
+    loadChildren: () => import('./report-incident/report-incident.module').then( m => m.ReportIncidentPageModule)
+  },
+
+
 ];
 
 @NgModule({
